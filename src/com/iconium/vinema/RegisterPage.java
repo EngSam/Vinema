7cc08aa6f9504e8a78717185a0099b683093fc1c
Samuel Mburu <smburu90@gmail.com>
2014-09-17 07:48:04 +0300

    These are the changes made to the initial application

---
+++ src/com/iconium/vinema/RegisterPage.java
@@ -0,0 +1,26 @@
+package com.iconium.vinema;
+
+import com.iconium.vinema.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterPage extends Activity {
+	
+	EditText username,password,password_reenter;
+	Button submit;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.registerpage);
+		submit=(Button)findViewById(R.id.buttonsubmit);
+		username=(EditText)findViewById(R.id.edittextusername);
+		password=(EditText)findViewById(R.id.registerpassword);
+		password_reenter=(EditText)findViewById(R.id.passwordrepeat);
+	}
+
+}
